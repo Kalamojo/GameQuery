ea94b2cbfc207319f39a74d7795db74db2ffa1eb
Kalamojo <kalabiprof@gmail.com>
2022-08-14 18:50:37 -0500

    db test

--- test3.py
+++ test3.py
@@ -1,17 +1,18 @@
 from flask import Flask, request, render_template, redirect
 from flask_sqlalchemy import SQLAlchemy
-from indexer import GameIndex
+import pickle
 from ranker import GameRank
-
-index = GameIndex()
-games, gameList, wordBank, textBank = index.games, index.gameList, index.wordBank, index.textBank
-ranks = GameRank(games, gameList, wordBank, textBank)
 
 test3 = Flask(__name__)
 test3.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///ranking.db'
 test3.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(test3)
 
+games = pickle.load(open("data/index.pkl", 'rb'))
+gameList = pickle.load(open("data/indexList.pkl", 'rb'))
+wordBank = pickle.load(open("data/wordBank.pkl", 'rb'))
+textBank = pickle.load(open("data/textBank.pkl", 'rb'))
+ranks = GameRank(games, gameList, wordBank, textBank)
 
 class Ranking(db.Model):
     id = db.Column(db.Integer, primary_key = True)
@@ -63,29 +64,6 @@ def index():
             print("Ayo")
             return render_template('index.html', ranks=query)
         return render_template('index.html', ranks=[])
-
-"""
-def index():
-    if request.method == 'POST':
-        query = request.form['content']
-        newRank = {}
-        if not query or query == '':
-            pass;
-        elif ranks.query_bool(query.que):
-            newRank = ranks.get_rank(ranks.querier(query.que))
-        else:
-            newRank = ranks.get_rank(ranks.rank1(query.que))
-        new_query = Ranking(que=query, rank=newRank)
-        try:
-            db.session.add(new_query)
-            db.session.commit()
-            return redirect('/')
-        except:
-            return "There was an issue fetching the games"
-    else:
-        query = Ranking.query.one_or_none()
-        return render_template('index.html', ranks=query)
-"""
 
 if __name__ == "__main__":
     test3.run(debug=True)
