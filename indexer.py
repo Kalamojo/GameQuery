f5b2866d6257e71b47b552c07ef130819ec76cfa
Kalamojo <kalabiprof@gmail.com>
2022-08-14 16:29:39 -0500

    Ranking custom library

---
+++ indexer.py
@@ -0,0 +1,64 @@
+from ntlkTools import tokeners
+import requests
+import json
+import pickle
+from pathlib import Path
+
+class GameIndex:
+    def __init__(self):
+        self.tokener = tokeners()
+        self.indexPath = "data/index.pkl"
+        self.listPath = "data/indexList.pkl"
+        self.wordPath = "data/wordBank.pkl"
+        self.textPath = "data/textBank.pkl"
+        self.ofs = 0
+        self.max_offset = 200500
+        self.offstep = 500
+        self.games, self.gameList, self.wordBank, self.textBank = self.newIndex()
+    
+    def newIndex(self):
+        games = {}
+        gameList = []
+        wordBank = []
+        textBank = []
+        indexFile = Path(self.indexPath)
+        listFile = Path(self.listPath)
+        wordFile = Path(self.wordPath)
+        textFile = Path(self.textPath)
+        if indexFile.is_file() and listFile.is_file() and wordFile.is_file() and textFile.is_file():
+            with open(self.indexPath, 'rb') as f:
+                    games = pickle.load(f)
+            with open(self.listPath, 'rb') as f:
+                    gameList = pickle.load(f)
+            with open(self.wordPath, 'rb') as f:
+                    wordBank = pickle.load(f)
+            with open(self.textPath, 'rb') as f:
+                    textBank = pickle.load(f)
+            print('load index: ' + self.indexPath + ', load list: ' + self.listPath)
+        else:
+            print('new index', 'new list')
+            while self.ofs <= self.max_offset:
+                if self.ofs % 10000 == 0:
+                    print(self.ofs)
+                r = requests.post("https://api.igdb.com/v4/games", headers={"Client-ID":"iandccesnxkg8eos8outaqkdjmcxkl", "Authorization":"Bearer uzjcv1er26qvwwyawqsqwjx4ghio13"}, 
+                                data=f"fields name, genres.name, themes.name, platforms.name, player_perspectives.name, game_modes.name, summary, url; limit {self.offstep}; offset {self.ofs};")
+                #jsonText = json.loads(r.text)
+                #if len(jsonText) == 0:
+                #    break;
+                gameList += json.loads(r.text)
+                self.ofs += self.offstep
+            print(self.ofs)
+            for c in range(len(gameList)):
+                games[c] = gameList[c]
+                gameList[c] = " ".join(self.tokener.bagOWords(games[c]))
+                wordBank[c] = set(self.tokener.bagOWords(games[c]))
+                textBank[c] = self.tokener.bagOWords(games[c])
+            with open(self.indexPath, 'wb') as f:
+                pickle.dump(games, f)
+            with open(self.listPath, 'wb') as f:
+                pickle.dump(gameList, f)
+            with open(self.wordPath, 'wb') as f:
+                pickle.dump(wordBank, f)
+            with open(self.textPath, 'wb') as f:
+                pickle.dump(textBank, f)
+        return games, gameList, wordBank, textBank
---
+++ ntlkTools.py
@@ -0,0 +1,68 @@
+from nltk.corpus import stopwords
+from nltk.stem.snowball import SnowballStemmer
+import re
+
+class tokeners:
+    def __init__(self, extra=['zero','one','two','three','four','five','six','seven',
+                   'eight','nine','ten','may','also','across','among','beside',
+                   'however','yet','within', 'game', 'player']): #"games", "game", "gaming", "gamer", "play", "playing"
+        self.stop_words = set(stopwords.words('english'))
+        self.stop_words.update(extra)
+        self.stemmer = SnowballStemmer("english")
+        self.re_stop_words = re.compile(r"\b(" + "|".join(self.stop_words) + ")\\W", re.I)
+
+    def tokenize(self, text):
+        words = re.findall(r'\w+', text)
+        words = [self.stemmer.stem(x.lower()) for x in self.re_stop_words.sub("", " ".join(words)).split(" ")]
+        return words
+
+    def tokening(self, text):
+        words = re.findall(r'\w+', text)
+        words = " ".join([self.stemmer.stem(x.lower()) for x in words])
+        return words
+
+    def bagOWords(self, curDict):
+        bow = []
+        if "summary" in curDict:
+            bow.append(curDict["summary"])
+        if "name" in curDict:
+            bow.append(curDict["name"])
+        if "game_modes" in curDict:
+            for mode in curDict["game_modes"]:
+                bow.append(mode["name"])
+        if "genres" in curDict:
+            for genre in curDict["genres"]:
+                bow.append(genre["name"])
+        if "themes" in curDict:
+            for theme in curDict["themes"]:
+                bow.append(theme["name"])
+        if "platforms" in curDict:
+            for platform in curDict["platforms"]:
+                bow.append(platform["name"])
+        if "player_perspectives" in curDict:
+            for pp in curDict["player_perspectives"]:
+                bow.append(pp["name"])
+        return self.tokenize(" ".join(bow))
+
+    def bagODetails(self, curDict):
+        bow = []
+        if "summary" in curDict:
+            bow = self.tokenize(curDict["summary"])
+        if "name" in curDict:
+            bow += self.tokenize(curDict["name"])
+        if "game_modes" in curDict:
+            for mode in curDict["game_modes"]:
+                bow.append(mode["name"])
+        if "genres" in curDict:
+            for genre in curDict["genres"]:
+                bow.append(genre["name"])
+        if "themes" in curDict:
+            for theme in curDict["themes"]:
+                bow.append(theme["name"])
+        if "platforms" in curDict:
+            for platform in curDict["platforms"]:
+                bow.append(platform["name"])
+        if "player_perspectives" in curDict:
+            for pp in curDict["player_perspectives"]:
+                bow.append(f"playerP{pp}")
+        return bow
