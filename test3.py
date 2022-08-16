844a361aff27f6854a7366bf07ffa8b272c19e68
Kalamojo <kalabiprof@gmail.com>
2022-08-16 11:11:49 -0500

    new databases

--- test3.py
+++ test3.py
@@ -8,42 +8,62 @@ test3 = Flask(__name__)
 test3.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///ranking.db'
 test3.config['SQLALCHEMY_BINDS'] = {
     'db1': 'sqlite:///ranking.db',
+    'db2': 'sqlite:///ranks.db',
+    'db3': 'sqlite:///index.db'
 }
 test3.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(test3)
 
-def getRank():
-    with open("data/index.json") as f:
-        games = json.load(f)
-    with open("data/indexList.json") as f:
-        gameList = json.load(f)
-    with open("data/wordBank.json") as f:
-        wordBank = json.load(f)
-#textBank = pickle.load(open("data/textBank.pkl", 'rb'))
-    return GameRank(games, gameList, wordBank)
+def getRank(a, b, c):
+    return GameRank(a, b, c)
 ranks = []
 class Ranking(db.Model):
     __bind_key__ = 'db1'
     id = db.Column(db.Integer, primary_key=True)
     que = db.Column(db.String(200), nullable=False)
     rank = db.Column(db.PickleType, default=[])
-
     def __repr__(self):
         return '<Query %r>' % self.id
 
-#class Ranks()
+class Ranks(db.Model):
+    __bind_key__ = 'db2'
+    id = db.Column(db.Integer, primary_key=True)
+    gameList = db.Column(db.PickleType, nullable=False)
+    wordBank = db.Column(db.PickleType, nullable=False)
+    def __repr__(self):
+        return '<Rank %r>' % self.id
 
+class Index(db.Model):
+    __bind_key__ = 'db3'
+    id = db.Column(db.Integer, primary_key=True)
+    games = db.Column(db.PickleType, nullable=False)
+    def __repr__(self):
+        return '<Index %r>' % self.id
+"""
 @test3.before_first_request
 def do_something_only_once():
     global ranks
     ranks = getRank()
+"""
 
 @test3.route('/', methods=['POST', 'GET'])
 def index():
+    """
+    new_Rank = Ranks(gameList=json.load(open("data/indexList.json")), 
+                    wordBank=json.load(open("data/wordBank.json")))
+    new_Index = Index(games=json.load(open("data/index.json")))
+    db.session.add(new_Index)
+    db.session.add(new_Rank)
+    db.session.commit()
+    """
     query = Ranking.query.one_or_none()
     if request.method == 'POST':
         print("pos")
         global ranks
+        if ranks == []:
+            rank = Ranks.query.one_or_none()
+            index = Index.query.one_or_none()
+            ranks = getRank(index.games, rank.gameList, rank.wordBank)
         if query:
             print("query")
             
@@ -62,6 +82,10 @@ def index():
                 return "There was an issue fetching the games"
         else:
             print("no query")
+            #new_Rank = Ranks(gameList=json.load(open("data/indexList.json")), 
+            #                wordBank=json.load(open("data/wordBank.json")))
+            #new_Index = Index(games=json.load(open("data/index.json")))
+            #ranks = getRank(new_Index.games, new_Rank.gameList, new_Rank.wordBank)
             query = request.form['content']
             newRank = []
             if not query or query == '':
@@ -72,6 +96,8 @@ def index():
                 newRank = ranks.get_rank(ranks.rank1(query))
             new_query = Ranking(que=query, rank=newRank)
             try:
+                #db.session.add(new_Index)
+                #db.session.add(new_Rank)
                 db.session.add(new_query)
                 db.session.commit()
                 return redirect('/')
@@ -81,7 +107,14 @@ def index():
         if query:
             print("Ayo")
             return render_template('index.html', ranks=query)
-        return render_template('index.html', ranks=[])
+        else:
+            new_Rank = Ranks(gameList=json.load(open("data/indexList.json")), 
+                            wordBank=json.load(open("data/wordBank.json")))
+            new_Index = Index(games=json.load(open("data/index.json")))
+            db.session.add(new_Index)
+            db.session.add(new_Rank)
+            db.session.commit()
+            return render_template('index.html', ranks=[])
 
 if __name__ == "__main__":
     test3.run(debug=True)
